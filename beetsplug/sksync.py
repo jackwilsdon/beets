7a045d22897caa3d4d73c302ae1400f2ac69afd8
Jack Wilsdon <jack.wilsdon@gmail.com>
2018-10-19 19:38:04 +0100

    [:warning: WIP] add more stuff

--- beetsplug/sksync.py
+++ beetsplug/sksync.py
@@ -22,14 +22,32 @@ from beets import autotag, library, ui, util
 from beets.autotag import hooks
 from collections import defaultdict
 import requests
+import urllib
+from urllib.parse import urlencode, parse_qs, urlsplit, urlunsplit
+
+def set_query_parameter(url, param_name, param_value):
+    """Given a URL, set or replace a query parameter and return the
+    modified URL.
+
+    >>> set_query_parameter('http://example.com?foo=bar&biz=baz', 'foo', 'stuff')
+    'http://example.com?foo=stuff&biz=baz'
+
+    """
+    scheme, netloc, path, query_string, fragment = urlsplit(url)
+    query_params = parse_qs(query_string)
+
+    query_params[param_name] = [param_value]
+    new_query_string = urlencode(query_params, doseq=True)
+
+    return urlunsplit((scheme, netloc, path, new_query_string, fragment))
 
 
 class SKSyncPlugin(BeetsPlugin):
 
     # Official SongKick API
     api_url = "https://api.songkick.com/api/3.0"
-    search_endpoint = "search/artists.json?api_key={api_key}&query={query}"
-    tracking_endpoint = "users/{username}/artists/tracked.json?apikey={api_key}&page={page}"
+    search_endpoint = "search/artists.json?apikey={api_key}&query={query}"
+    tracking_endpoint = "users/{username}/artists/tracked.json?apikey={api_key}"
 
     # Unofficial SongKick API
     site_url = "https://www.songkick.com"
@@ -70,52 +88,109 @@ class SKSyncPlugin(BeetsPlugin):
         if username is None:
             raise ui.UserError("missing username")
 
-        songkick_aritsts = self.get_songkick_artists(api_key, username)
-        local_artists = self.get_artists(lib)
-        new_artists = [ artist for artist in local_artists if artist not in songkick_aritsts ]
+        local_artists = self.get_library_artists(lib)
+        songkick_artists = self.get_songkick_artists(api_key, username)
+
+        # Fetch a list of local artists who aren't in the remote artist list.
+        new_artists = dict((mbid, name) for mbid, name in local_artists.items() if mbid not in songkick_artists)
 
         ui.print_("Found {} new artist(s) to sync".format(len(new_artists)))
 
-        for artist in new_artists:
+        # If there's no new artists then there's nothing to do.
+        if len(new_artists) == 0:
+            return
+
+        for artist in new_artists.values():
             ui.print_(" - " + artist)
 
         if not ui.input_yn(u"Sync? (Y/n)"):
             return
 
-    def get_artists(self, lib):
-        return set(x.artist for x in lib.items())
+        for mbid, artist in new_artists.items():
+            self._log.debug("Searching for {}...", artist)
 
-    def get_songkick_artists(self, api_key, username):
-        def get_page_artists(page):
-            url = (self.api_url + "/" + self.tracking_endpoint).format(api_key=api_key, username=username, page=page)
+            results = self.search_songkick_artists(api_key, artist)
 
-            self._log.debug("making request for page {}: {}", page, url)
 
-            response = requests.get(url).json()["resultsPage"]
+            print(results)
+            print(mbid)
 
+            if mbid not in results:
+                self._log.debug("Couldn't find matching artist with MBID {}", mbid)
+                continue
+
+            print("Found match!")
+
+    @staticmethod
+    def get_library_artists(lib):
+        return dict((x.mb_artistid, x.artist) for x in lib.items())
+
+    def paginated_request(self, url, result_key):
+        page = 1
+        results = []
+
+        while True:
+            page_url = set_query_parameter(url, 'page', page)
+
+            self._log.debug("making request for page {}: {}", page, page_url)
+
+            # Every response should contain a results page.
+            response = requests.get(page_url).json()["resultsPage"]
+
+            # Every response should have a status attribute.
             if response["status"] != "ok":
                 raise ui.UserError("status not OK from API: {}".format(response["error"]["message"]))
 
-            if "artist" not in response["results"]:
-                self._log.debug("got 0 artists")
-                return None
-
-            self._log.debug("got {} artists", len(response["results"]["artist"]))
-
-            return set(artist["displayName"] for artist in response["results"]["artist"])
-
-        artists = []
-        page = 1
-
-        while True:
-            page_artists = get_page_artists(page)
-
-            if page_artists is None:
+            # Stop if the key doesn't exist, as this usually means it's the last page.
+            if result_key not in response["results"]:
+                self._log.debug("got 0 {}s on page {}", result_key, page)
                 break
 
-            artists += page_artists
-            page += 1
+            self._log.debug("got {} {}s on page {}", len(response["results"][result_key]), result_key, page)
 
-        self._log.debug("got {} artists over {} page(s)", len(artists), page - 1)
+            page += 1
+            results += response["results"][result_key]
+
+        self._log.debug("got {} {}s across {} page(s)", len(results), result_key, page)
+
+        return results
+
+    def get_songkick_artists(self, api_key, username):
+        url = (self.api_url + "/" + self.tracking_endpoint).format(
+            api_key=urllib.parse.quote_plus(api_key),
+            username=urllib.parse.quote_plus(username))
+
+        artists = {}
+
+        for artist in self.paginated_request(url, "artist"):
+            name = artist["displayName"]
+
+            if len(artist["identifier"]) == 0:
+                self._log.debug("missing identifier for artist {}", name)
+                continue
+
+            # XXX: Just use the first MBID returned from the service (is this a good idea?)
+            artists[artist["identifier"][0]["mbid"]] = namey
 
         return artists
+
+    def search_songkick_artists(self, api_key, query):
+        url = (self.api_url + "/" + self.search_endpoint).format(
+            api_key=urllib.parse.quote_plus(api_key),
+            query=urllib.parse.quote_plus(query))
+
+        artists = {}
+
+        for artist in self.paginated_request(url, "artist"):
+            name = artist["displayName"]
+
+            if len(artist["identifier"]) == 0:
+                self._log.debug("missing identifier for artist {}", name)
+                continue
+
+            # XXX: Just use the first MBID returned from the service (is this a good idea?)
+            artists[artist["identifier"][0]["mbid"]] = name
+
+        return artists
+
+
